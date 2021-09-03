0598453f73b498461b7ec7cd1f632da7128f222e
Kai Andre Hovden <kai.hovden@bouvet.no>
2021-09-03 13:34:39 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';  
+import ReactDOM from 'react-dom';  
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './App';  
+import { Provider } from 'react-redux';  
+import { createStore, applyMiddleware } from 'redux';  
+import thunk from "redux-thunk";  
+import reducer from './Redux/reducer';
+  
+const store = createStore(reducer, applyMiddleware(thunk));  
+  
+ReactDOM.render(  
+    <Provider store={store}>  
+        <App />  
+    </Provider>, document.getElementById('root'));  
+  
+// If you want your app to work offline and load faster, you can change    
+// unregister() to register() below. Note this comes with some pitfalls.    
+// Learn more about service workers: http://bit.ly/CRA-PWA    
